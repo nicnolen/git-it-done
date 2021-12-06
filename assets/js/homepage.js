c67884c73392fefdcd146a0f4c61e720ae096e71
Nicholas Nolen <nicnolen@ymail.com>
2021-12-06 10:28:29 -0600

    read the data that was returned in GitHub's server response

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,8 +1,15 @@
 // FUNCTIONS
 // Search for GitHub users and list all their repositories
 var getUserRepos = function () {
-    // fetch resources by making an HTTP request to GitHub's web server
+    // make an HTTP request to GitHub's web server. In response, Github's server gave us JSON data.
     fetch("https://api.github.com/users/octocat/repos");
+
+    // read the data that was return in Github's server response
+    var response = fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
 getUserRepos();
